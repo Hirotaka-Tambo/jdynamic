5e222e047a1acd6c432e1480363e770532c19c55
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-08-13 00:25:37 +0900

    9章復習その１

---
+++ src/main/java/la/dao/DAOException.java
@@ -0,0 +1,11 @@
+package la.dao;
+
+/**
+ * Servlet implementation class DAOException
+ */
+
+public class DAOException extends Exception{
+	public DAOException(String message) {
+		super(message);
+	}
+}
