27e1c04563cd4b7bb8b28c6166b94d4475f49e69
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-08-14 00:14:25 +0900

    9章復習その2

---
+++ src/main/java/la/servlet/ItemServlet.java
@@ -0,0 +1,53 @@
+package la.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import la.bean.ItemBean;
+import la.dao.DAOException;
+import la.dao.ItemDAO;
+
+/**
+ * Servlet implementation class ItemServlet
+ */
+@WebServlet("/ItemServlet")
+public class ItemServlet extends HttpServlet {
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			ItemDAO dao = new ItemDAO();
+			List<ItemBean> list = dao.findAll();
+			
+			request.setAttribute("items", list);
+			RequestDispatcher rd= request.getRequestDispatcher("/showItem.jsp");
+			rd.forward(request,response);
+			
+		}catch (DAOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			request.setAttribute("message", "内部エラーが発生しました");
+			RequestDispatcher rd = request.getRequestDispatcher("/errInternal.jsp");
+			rd.forward(request,response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
